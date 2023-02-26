5fea2059e72c904802e00d5c4ff110e070945a65
Javohir <narzullayevj99@gmail.com>
2023-02-26 06:08:37 +0500

    init

--- swagger-diff/src/main/java/uz/narzullayev/javohir/OpenApiDiffMojo.java
+++ swagger-diff/src/main/java/uz/narzullayev/javohir/OpenApiDiffMojo.java
@@ -1,18 +1,22 @@
 package uz.narzullayev.javohir;
 
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import uz.narzullayev.javohir.model.ChangedOpenApi;
-import uz.narzullayev.javohir.output.ConsoleRender;
 import uz.narzullayev.javohir.output.HtmlRender;
 import uz.narzullayev.javohir.output.MarkdownRender;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 /** A Maven Mojo that diffs two OpenAPI specifications and reports on differences. */
 @Mojo(name = "diff", defaultPhase = LifecyclePhase.TEST)
@@ -30,6 +34,16 @@ public class OpenApiDiffMojo extends AbstractMojo {
   Boolean failOnChanged = false;
 
 
+  @Component
+  protected MavenProject project;
+
+  @Parameter(defaultValue = "${project.basedir}/src/main/resources", required = true, readonly = true)
+  private String sourceResourceDir;
+
+  @Parameter( defaultValue = "${localRepository}", readonly = true, required = true )
+  protected ArtifactRepository localRepository;
+
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     try {
@@ -44,8 +58,11 @@ public class OpenApiDiffMojo extends AbstractMojo {
               "http://deepoove.com/swagger-diff/stylesheets/demo.css")
               .render(diff);
 
+
+      getLog().error("Error start " +sourceResourceDir);
+      getLog().error("Local start " +localRepository.getUrl());
       try {
-        FileWriter fw = new FileWriter("testNewApi.html");
+        var fw = new FileWriter(sourceResourceDir+"/templates/changelog.html");
         fw.write(html);
         fw.close();
       } catch (IOException e) {

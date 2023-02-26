9d4658a06b8bc60d59f093d070a8b790a056ba27
Javohir <narzullayevj99@gmail.com>
2023-02-26 17:20:46 +0500

    init

--- swagger-diff/src/main/java/uz/narzullayev/javohir/OpenApiDiffMojo.java
+++ swagger-diff/src/main/java/uz/narzullayev/javohir/OpenApiDiffMojo.java
@@ -1,7 +1,6 @@
 package uz.narzullayev.javohir;
 
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -10,9 +9,7 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import uz.narzullayev.javohir.model.ChangedOpenApi;
 import uz.narzullayev.javohir.output.HtmlRender;
-import uz.narzullayev.javohir.output.MarkdownRender;
 
 import java.io.FileWriter;
 import java.io.IOException;
